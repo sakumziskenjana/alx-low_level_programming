4c02058169f885712c4f00d202144516d29c49c7
Sakumzi <skenjanakeith@gmail.com>
2022-10-25 02:02:05 -0700

    0

---
+++ 0-print_listint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * print_list - prints all the elemtns of a listint_t list
+ * @h: a pointer to the head of th elist_t list
+ *
+ * Return: The number of nodes in this list_t list
+ */
+size_t print_listint(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h)
+	{
+		nodes++;
+		printf("%d\n", h->n);
+		h - h->next;
+	}
+
+	return (nodes);
