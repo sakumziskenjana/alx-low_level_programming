a96ef978546b80ea2512e432c898ddae20187b66
Sakumzi <skenjanakeith@gmail.com>
2022-10-25 09:38:58 -0700

    2

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -14,7 +14,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
-	new = malloc(sizeof(listint_t _t));
+	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
 
