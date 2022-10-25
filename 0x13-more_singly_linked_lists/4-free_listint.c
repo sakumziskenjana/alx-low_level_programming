4d99bc9922d95f894faf815b846b8dfdb0247165
Sakumzi <skenjanakeith@gmail.com>
2022-10-25 10:01:54 -0700

    4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t list
+ * @head: a pointer to the address of the listint_t lsy to be freed
+ */
+void free_listint(listint_t *head)
+{
+	listint_t *tmp;
+
+	while (head)
+	{
+		*tmp = head->next;
+		free(head);
+		head = tmp;
+	}
+}
