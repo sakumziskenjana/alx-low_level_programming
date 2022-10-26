f1bedc49dff5ce39bc8a595e6e3a655026d6e87b
Sakumzi <skenjanakeith@gmail.com>
2022-10-26 13:43:40 -0700

    7

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,4 @@
+#include "lists.h"
+
+/**
+ *  delete_nodeint_at_index - A function that deletes
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -7,6 +7,7 @@
  * @index: the returning index
  * Return: NUll
  */
+
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i = 0;
@@ -22,5 +23,5 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		i++;
 	}
 
-	return (NUll)
+	return (NULL)
 }
