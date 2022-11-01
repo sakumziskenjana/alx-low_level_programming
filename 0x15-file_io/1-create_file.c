10364fc0f5d35ab57f182f6f0d644289c0467767
Sakumzi <skenjanakeith@gmail.com>
2022-11-01 03:53:44 -0700

    1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,29 +1,32 @@
 #include "main.h"
 
 /**
- * create_file - ...
+ * create_file - creates a file
  * @filename: The file name
- * @text_context: ...
+ * @text_context: A pointer to a string to write in the file
  *
  * Return: 1 on success, -1 on failure
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int o, w, len = 0;
 
-	if (!filename)
+	if (!filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
+	if (text_content != NULL)
+	{
+		for (len = 0; text_context[len];)
+			len++;
+	}
 
-	if (fd == -1)
+	o = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	w = write(o, text_context, len);
+
+	if (o == -1 || w == -1)
 		return (-1);
 
-	if (text_content)
+	close(o);
 
-		write(fd, text_content,
-				_strlrn(text_content));
-
-	close(fd);
 	return (1);
 }
